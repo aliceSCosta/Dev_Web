ca71276a303eaae88abb49d418bb24219e34274d
alicesilva <alicealmeyda72@gmail.com>
2019-12-13 17:23:43 -0300

    .

--- Salmo/organizar/modalImage/modal.js
+++ Salmo/organizar/modalImage/modal.js
@@ -1,27 +1,12 @@
 $(document).ready(function(){
 
-    // Get the modal
-    //var modal = $("myModal");
+    $("#myImg").click(function(){
+        $("#myModal").show();
+        $(".modal-content").attr("src", $(this).attr("src"));
+        $("#caption").text($(this).attr("alt"));
+    });
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    //var img = $("myImg");
-    //var modalImg = $("nebulosa01");
-    //var captionText = $("caption");
-    $(".myImg").click(function(){
-        $(".myModal")
+    $(".close").click(function(){
+        $(".modal").hide();
     })
-    //img.onclick = function(){
-    //modal.style.display = "block";
-    //modalImg.src = this.src;
-    //captionText.innerHTML = this.alt;
-    //}
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
-
 })
